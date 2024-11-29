d4bb01b3084485994027ea7666fc5be6ec7ef361
Onkar Giri <145758938+Onkargiri29@users.noreply.github.com>
2024-11-29 12:34:46 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -86,4 +86,4 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if _name_ == '_main_':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
